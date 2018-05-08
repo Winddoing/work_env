699b181cc7c147fe4d63d0a4ac18b67e5b7f0bb0
winddoing <winddoing@sina.com>
2018-05-08 20:25:25 +0800

    add tftp

---
+++ prepare.sh
@@ -0,0 +1,17 @@
+##########################################################
+# File Name		: prepare.sh
+# Author		: wqshao
+# Created Time	: 2018年05月07日 星期一 19时30分14秒
+# Description	:
+##########################################################
+#!/bin/bash
+
+sudo apt-get install  git \
+	tmux \
+	meld \
+	vim  \
+	ctags \
+	subversion \
+	sshfs \
+	tftpd tftp xinetd 
+
---
+++ tftp/install.sh
@@ -0,0 +1,24 @@
+##########################################################
+# File Name		: install.sh
+# Author		: wqshao
+# Created Time	: 2018年05月08日 星期二 15时34分02秒
+# Description	:
+##########################################################
+#!/bin/bash
+
+PWD=`pwd`
+user=$USER
+home=$HOME
+
+
+sudo apt-get install tftpd tftp xinetd
+
+
+mkdir $home/tftprootfs
+sudo chmod 777 $home/tftprootfs -R
+
+sudo cp tftp  /etc/xinetd.d
+
+sudo /etc/init.d/xinetd restart
+
+
