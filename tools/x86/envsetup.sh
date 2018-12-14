1809f469dcfe25b1e7b68efb2e969261db5697ce
winddoing <winddoing@sina.com>
2018-12-14 09:52:14 +0800

    add sparse and delete envsetup.sh

--- tools/x86/envsetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-PATH=/home_back/wqshao/bin:/bin:/usr/bin:/usr/local/bin:/pdev/chains/mips-gcc472-glibc216/bin
-PATH=$PATH:/pdev/chains/mipsel-linux-android-4.7/bin
-
-echo "$PATH"
-
-export
