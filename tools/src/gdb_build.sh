74ccd9209b31a1041b6ede7f407db9350e4749ff
shaowangquan <shaowangquan@vanxum.com>
2019-03-14 11:02:56 +0800

    fix gdb build script

--- tools/src/gdb_build.sh
+++ tools/src/gdb_build.sh
@@ -7,6 +7,7 @@
 #!/bin/bash
 
 PWD=`pwd`
+CROSS_GCC_CC=aarch64-linux-gcc
 DEFAULT_TARGET_ARRAY=(arm-linux aarch64-linux)
 DEFAULT_TARGET_CNT=${#DEFAULT_TARGET_ARRAY[@]}
 
@@ -32,6 +33,10 @@ else
     INSTALL_DIR="_${TARGET}_install"
 fi
 
+if [ ! -z $CROSS_GCC_CC ]; then
+    INSTALL_DIR="_cross_${INSTALL_DIR}"
+fi
+
 echo "==> $0 $1 $# $INSTALL_DIR $DEFAULT_TARGET_CNT"
 
 mkdir tmp
@@ -47,7 +52,12 @@ tar zxvf gdb-8.2.tar.gz
 
 cd gdb-8.2
 
-./configure --target=$TARGET --prefix=${INSTALL_PATH}  --enable-static
+if [ -z $CROSS_GCC_CC]; then
+    ./configure --target=$TARGET --prefix=${INSTALL_PATH}  --enable-static
+else
+    ./configure CC=${CROSS_GCC_CC} --target=${TARGET} --host=${TARGET} --prefix=${INSTALL_PATH}  --enable-static
+fi
+
 make -j2
 make install
 
@@ -58,4 +68,6 @@ cd .. #tmp
 
 echo "INSTALL_PATH: ${INSTALL_PATH}"
 
-cp ${INSTALL_PATH}/bin/* ../x86 -v
+if [ -z ${CROSS_GCC_CC} ]; then
+    cp ${INSTALL_PATH}/bin/* ../x86 -v
+fi
