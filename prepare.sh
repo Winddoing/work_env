73ff4c94f4cdbb7a9e8d6b40d264ce369976fb8e
winddoing <winddoing@sina.cn>
2019-12-18 20:51:26 +0800

    add mpstat

--- prepare.sh
+++ prepare.sh
@@ -25,7 +25,8 @@ LINUX_BASEL_SOFTWARE=(git git-extras tig vim tmux exuberant-ctags cscope doxygen
     openssh-server samba smbclient htop gcc g++ make cmake net-tools graphviz
     tree colordiff subversion tftpd tftp xinetd sshfs minicom adb astyle splint
     cloc sparse fakeroot icdiff indent cgdb tldr repo fcitx-bin fcitx-table
-    cpustat cpufrequtils linux-tools-generic apitrace apitrace-gui speedtest-cli)
+    cpustat cpufrequtils linux-tools-generic apitrace apitrace-gui speedtest-cli
+    sysstat)
 
 LINUX_GRAPH_SOFTWARE=(gitk meld eog cutecom deepin-screenshot firefox vlc
     kolourpaint rapidsvn thunderbird ksysguard gnome-tweaks simplescreenrecorder)
