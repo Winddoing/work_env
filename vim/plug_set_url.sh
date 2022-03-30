40bd5aa3fc943498fdb936397c2e0ef4e5e3291f
winddoing <winddoing@sina.cn>
2022-03-30 16:30:32 +0800

    [vim] add plug_set_url.sh

---
+++ vim/plug_set_url.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+old_plug_url="https://git::@hub.fastgit.xyz"
+new_plug_url="https://github.com"
+
+for plug in `ls ./plugged`
+do
+	echo "Plug dir: $plug"
+	cd ./plugged
+	cd $plug
+
+	old_url=$(git remote -v | grep fetch | awk '{print $2}')
+	echo "old url: $old_url"
+	new_url=${old_url/$old_plug_url/$new_plug_url}
+	echo "new url: $new_url"
+	git remote set-url origin $new_url
+	if [ $? -eq 0 ]; then
+		echo "OK"
+	else
+		echo "Err"
+	fi
+	cd ..
+	cd ..
+done
